b70598adf51bbab5a3efc3aa3398e7171537ad0f
* <gsbenevides2@gmail.com>
2020-02-01 14:08:07 -0300

    update to heroku

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
+let baseURL
+if(window.location.hostname === "omnistack-10-gsb.netlify.com"){
+ baseURL = "https://omnistack-10-gsb.herokuapp.com"
+}
+else{
+ baseURL = 'http://localhost:3333/'
+}
+
+
 const api = axios.create({
- baseURL: 'http://localhost:3333/'
+ baseURL
 })
 
 export default api
